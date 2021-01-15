ccfad8c8163f8c11f078836b1ee11f1f8bac4f4f
Efrain May <efrain_may_75@hotmail.com>
2021-01-15 16:29:34 -0600

    login bloc - crontolando los campos del formulario

---
+++ lib/src/blocs/login_bloc.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+class LoginBloc {
+
+  final _emailController = StreamController<String>.broadcast();
+  final _passwordController = StreamController<String>.broadcast();
+
+  //Recuperar los datos de String en la salida
+  Stream<String> get emailStream => _emailController.stream;
+  Stream<String> get passwordStream => _passwordController.stream;
+
+  //Insertar valores al String
+  Function(String) get changeEmail => _emailController.sink.add;
+  Function(String) get changePassword => _passwordController.sink.add;
+
+  dispose(){
+    _emailController?.close();
+    _passwordController?.close();
+  }
+}
