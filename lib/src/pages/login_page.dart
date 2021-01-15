ed2b502a5e3735d2b129c816170d68bab1ece4a1
Efrain May <efrain_may_75@hotmail.com>
2021-01-15 16:15:00 -0600

    formulario maquetado

--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -7,8 +7,99 @@ class LoginPage extends StatelessWidget {
       body: Stack(
         children: <Widget>[
           _crearFondo(context),
+          _loginForm(context),
         ],
       )
+    );
+  }
+
+  Widget _loginForm(BuildContext context){
+
+    final size = MediaQuery.of(context).size;
+
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          SafeArea(
+            child:  Container(
+              height: 180.0,
+            ),
+          ),
+          Container(
+            width: size.width * 0.85,
+            margin: EdgeInsets.symmetric(vertical: 30.0),
+            padding: EdgeInsets.symmetric(vertical: 50.0),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(5.0),
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 3.0,
+                  offset: Offset(0.0, 5.0),
+                  spreadRadius: 3.0
+                )
+              ]
+            ),
+            child: Column(
+              children: <Widget>[
+                Text('Ingreso', style: TextStyle(fontSize: 20.0)),
+                SizedBox(height: 60.0),
+                _crearEmail(),
+                SizedBox(height: 30.0),
+                _crearPassword(),
+                SizedBox(height: 60.0),
+                _crearBoton(),
+              ],
+            ),
+          ),
+          Text('¿Olvido la ciontraseña?'),
+          SizedBox(height: 100.0)
+        ],
+      ),
+    );
+  }
+
+  Widget _crearEmail(){
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: TextField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          icon: Icon(Icons.alternate_email, color: Colors.deepPurple),
+          hintText: 'ejemplo@correo.com',
+          labelText: 'Correo electronico'
+        ),
+      ),
+    );
+  }
+
+  Widget _crearPassword(){
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+          icon: Icon(Icons.lock_outline, color: Colors.deepPurple),
+          labelText: 'Contraseña'
+        ),
+      ),
+    );
+  }
+
+  Widget _crearBoton(){
+    return RaisedButton(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
+        child: Text('Ingresar'),
+      ),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(5.0)
+      ),
+      elevation: 0.0,
+      color: Colors.deepPurple,
+      textColor: Colors.white,
+      onPressed: (){},
     );
   }
 
