2b28ed4e61e80ad67986d54ab2656cee46258ef7
Efrain May <efrain_may_75@hotmail.com>
2021-01-16 02:51:21 -0600

    rxdart agregado para validar el boton si los campos se llenan en el form

--- lib/src/blocs/login_bloc.dart
+++ lib/src/blocs/login_bloc.dart
@@ -1,14 +1,17 @@
 import 'dart:async';
 import 'package:formvalidation/src/blocs/validators.dart';
+import 'package:rxdart/rxdart.dart';
 
 class LoginBloc with Validators{
 
-  final _emailController = StreamController<String>.broadcast();
-  final _passwordController = StreamController<String>.broadcast();
+  final _emailController = BehaviorSubject<String>();
+  final _passwordController = BehaviorSubject<String>();
 
   //Recuperar los datos de String en la salida
   Stream<String> get emailStream => _emailController.stream.transform(validarEmail);
   Stream<String> get passwordStream => _passwordController.stream.transform(validarPassword);
+
+  Stream<bool> get formValidStream => Rx.combineLatest2(emailStream, passwordStream, (e, p) => true);
 
   //Insertar valores al String
   Function(String) get changeEmail => _emailController.sink.add;
--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -51,7 +51,7 @@ class LoginPage extends StatelessWidget {
                 SizedBox(height: 30.0),
                 _crearPassword(bloc),
                 SizedBox(height: 60.0),
-                _crearBoton(),
+                _crearBoton(bloc),
               ],
             ),
           ),
@@ -108,19 +108,25 @@ class LoginPage extends StatelessWidget {
     );
   }
 
-  Widget _crearBoton(){
-    return RaisedButton(
-      child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
-        child: Text('Ingresar'),
-      ),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(5.0)
-      ),
-      elevation: 0.0,
-      color: Colors.deepPurple,
-      textColor: Colors.white,
-      onPressed: (){},
+  Widget _crearBoton(LoginBloc bloc){
+
+    return StreamBuilder(
+      stream: bloc.formValidStream,
+      builder: (BuildContext context, AsyncSnapshot snapshot){
+        return RaisedButton(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
+            child: Text('Ingresar'),
+          ),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(5.0)
+          ),
+          elevation: 0.0,
+          color: Colors.deepPurple,
+          textColor: Colors.white,
+          onPressed: snapshot.hasData ? (){} : null,
+        );
+      }
     );
   }
 
