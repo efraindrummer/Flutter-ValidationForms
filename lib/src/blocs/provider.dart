4e8b47b7a93367a033105f69234ad41b616c4b9e
Efrain May <efrain_may_75@hotmail.com>
2021-01-16 13:27:37 -0600

    se mantuvo la data de los streams despues de hacer el hot reload

--- lib/src/blocs/provider.dart
+++ lib/src/blocs/provider.dart
@@ -1,12 +1,25 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:formvalidation/src/blocs/login_bloc.dart';
 export 'package:formvalidation/src/blocs/login_bloc.dart';
 
 class Provider extends InheritedWidget{
 
+  static Provider _instancia;
+
+  factory Provider({Key key, Widget child}){
+    if(_instancia == null){
+      _instancia = new Provider._internal(key: key, child: child);
+    }
+
+    return _instancia;
+  }
+
+  Provider._internal({Key key, Widget child}) : super(key: key, child: child);
+
   final loginBloc = LoginBloc();
 
-  Provider({Key key, Widget child}) : super(key: key, child: child);
+  //Provider({Key key, Widget child}) : super(key: key, child: child);
 
   @override
   bool updateShouldNotify(covariant InheritedWidget oldWidget) => true;
