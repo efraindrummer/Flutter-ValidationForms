9419e5c0171335ca1a41282a423a9fb1214238b7
Efrain May <efrain_may_75@hotmail.com>
2021-01-17 19:41:04 -0600

    productos bloc añadido al inhereited widget

--- lib/src/blocs/provider.dart
+++ lib/src/blocs/provider.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:formvalidation/src/blocs/login_bloc.dart';
+import 'package:formvalidation/src/blocs/productos_bloc.dart';
+export 'package:formvalidation/src/blocs/productos_bloc.dart';
 export 'package:formvalidation/src/blocs/login_bloc.dart';
 
 class Provider extends InheritedWidget{
+
+  final loginBloc      = new LoginBloc();
+  final _productosBloc = new ProductosBloc();
 
   static Provider _instancia;
 
@@ -17,8 +22,6 @@ class Provider extends InheritedWidget{
 
   Provider._internal({Key key, Widget child}) : super(key: key, child: child);
 
-  final loginBloc = LoginBloc();
-
   //Provider({Key key, Widget child}) : super(key: key, child: child);
 
   @override
@@ -28,4 +31,7 @@ class Provider extends InheritedWidget{
     return context.dependOnInheritedWidgetOfExactType<Provider>().loginBloc;
   }
 
+  static ProductosBloc productosBloc (BuildContext context){
+    return context.dependOnInheritedWidgetOfExactType<Provider>()._productosBloc;
+  }
 }
